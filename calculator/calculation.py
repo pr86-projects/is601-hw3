7a1e09291314ff506ab5306658c559ddd5838d95
pr86projects <pr86@njit.edu>
2024-02-11 01:07:46 -0500

    refactored the calculation to move it to ots own file

---
+++ calculator/calculation.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+from typing import Callable
+from calculator.operations import add, subtract,multiply, divide
+
+# Calculation class 
+class Calculation:
+    def __init__(self, a: Decimal, b: Decimal, operation):
+        # Dunder method for object initialization
+        self.a = a # property, stores value of a in an instance of this class
+        self.b = b # property, stores value of b in an instance of this class
+        self.operation = operation # property, stores the operation function
+
+    def get_result(self):
+        # Calls stored operation with a and b
+        return self.operation(self.a, self.b)
